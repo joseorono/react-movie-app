a1b2f823b861121c9fd8ceb6e2f25f274ae9f4f0
José Miguel Oroño <joseomaker@gmail.com>
2023-09-09 14:54:48 -0500

    Add custom "cinemared" dark DaisyUI theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -106,9 +106,46 @@ export default {
   variants: {
     textColor: ['responsive', 'hover', 'focus', 'group-hover'],
   },
-  plugins: [
-    require('@tailwindcss/typography'),
-    require("daisyui")
-  ]
-}
-
+  daisyui: {
+    themes: [
+      {
+        'redcinema': {
+          'primary': '#f21717',
+          'primary-focus': '#cb0c0c',
+          'primary-content': '#131616',
+          'secondary': '#ffd700',
+          'secondary-focus': '#F5D000',
+          'secondary-content': '#ffffff',
+          'accent': '#4fa300',
+          'accent-focus': '#367000',
+          'accent-content': '#ffffff',
+          'neutral': '#1b1d1d',
+          'neutral-focus': '#131616',
+          'neutral-content': '#ffffff',
+          'base-100': '#1f1f1f',
+          'base-200': '#1b1d1d',
+          'base-300': '#131616',
+          'base-content': '#ffffff',
+          'info': '#66c7ff',
+          'success': '#87cf3a',
+          'warning': '#e1d460',
+          'error': '#ff6b6b',
+          '--rounded-box': '1rem',
+          '--rounded-btn': '.5rem',
+          '--rounded-badge': '1.9rem',
+          '--animation-btn': '.25s',
+          '--animation-input': '.2s',
+          '--btn-text-case': 'uppercase',
+          '--navbar-padding': '.5rem',
+          '--border-btn': '1px',
+        }
+      },
+        "business",
+        "fantasy"
+      ],
+    },
+    plugins: [
+      require('@tailwindcss/typography'),
+      require("daisyui")
+    ]
+  }
