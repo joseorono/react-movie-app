6c35951a306e4e2cf0e7d79e9c74d11d3b4a6962
José Miguel Oroño <joseomaker@gmail.com>
2023-09-13 12:50:13 -0500

    Improve MovieDetails and add Trailers

--- src/components/MovieDetails.tsx
+++ src/components/MovieDetails.tsx
@@ -41,12 +41,13 @@ export default function MovieDetails({ movieId }: any) {
         )
     } 
 
+    let backdrop_url = `url(https://image.tmdb.org/t/p/original/${data.backdrop_path})`
     // Else, render data...
     return (
     <>
         
         {/* Hero to Show Case the Movie's Title */}
-        <div className="hero max-h-[400px] h-screen" style={{backgroundImage: 'url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg)'}}>
+        <div className="hero max-h-[400px] h-screen" style={{backgroundImage: backdrop_url}}>
             <div className="hero-overlay bg-opacity-60"></div>
             <div className="hero-content text-center text-neutral-content">
             <div className="max-w-md">
@@ -55,36 +56,47 @@ export default function MovieDetails({ movieId }: any) {
             </div>
         </div>
 
-        {/* Main Section */}
-        <div className="movie-details">
-            <h3>{data.title}</h3>
-            <p>{data.overview}</p>
-            <p>Release: {data.release_date}</p>
-            <p>Score: {data.vote_average}/10</p>
-            <p>Length: {data.runtime}min</p>
-            {/* Agrega más detalles aquí según tus necesidades */}
-        </div>
-
-        {/* Movie Videos */}
-
-        <div className="movie-videos">
-            <h3>Trailer</h3>
-            {
-                data.videos.results.filter( (video: any) => {
-                    return video.type === "Trailer"
-                }).map(
-                    (trailer: any, index: number) => {
-                        return (
-                            <div className='text-center mt-4' key={"movieTrailer-"+index}>
-                                <iframe width="560" height="315" src={`https://www.youtube.com/embed/${trailer.key}`} title={trailer.name} allowFullScreen></iframe>
-                            </div>
-                        )
-                })
-            }
+        <main className='container md:mx-auto md:w-10/12 xl:w-9/12 py-4 px-4'>
             
-        </div>
+            {/* Main Section */}
+            <div className="movie-details">
+                <h3>{data.title}</h3>
+                <p>{data.overview}</p>
+                <p>Release: {data.release_date}</p>
+                <p>Score: {data.vote_average}/10</p>
+                <p>Length: {data.runtime}min</p>
+                {/* Agrega más detalles aquí según tus necesidades */}
+            </div>
 
-        {/* Movie Images */}
+            {/* Movie Videos */}
+
+            <div className="movie-videos">
+                <h3>Trailer</h3>
+                {
+                    data.videos.results.filter( (video: any) => {
+                        return video.type === "Trailer"
+                    }).map(
+                        (trailer: any, index: number) => {
+                            let base_url = "";
+                            if (trailer.site === "YouTube") {
+                                base_url = "https://www.youtube.com/embed/";
+                            } else if (trailer.site === "Vimeo") {
+                                base_url = "https://player.vimeo.com/video/";
+                            }
+                            let trailer_url = `${base_url}${trailer.key}`;
+                            return (
+                                <div className='text-center mt-4 flex justify-center' key={"movieTrailer-"+index}>
+                                    <iframe width="560" height="315" src={trailer_url} title={trailer.name} allowFullScreen></iframe>
+                                </div>
+                            )
+                    })
+                }
+                
+            </div>
+
+            {/* Movie Images */}
+
+        </main>
 
     </>
     );
