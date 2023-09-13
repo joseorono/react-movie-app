cab53966f7e8d52368c40e7e47df557016ea90c0
José Miguel Oroño <joseomaker@gmail.com>
2023-09-13 12:11:12 -0500

    Clean up MovieDetailsPage

--- src/pages/MovieDetailsPage.tsx
+++ src/pages/MovieDetailsPage.tsx
@@ -1,8 +1,8 @@
 
 
 import { Suspense } from 'react'
-import useSWR from 'swr'
-import { Routes, Route, useParams } from 'react-router-dom';
+
+import { useParams } from 'react-router-dom';
 
 import MovieLoader from "../components/loaders/MovieLoader";
 import MovieDetails from '../components/movieDetails';
@@ -14,16 +14,6 @@ export type MovieParams = {
 export default function MovieDetailsPage() {
 
   let { movieId } = useParams<keyof MovieParams>() as MovieParams;
-
-  const movie = {
-    title: "The Lord of the Rings: The Fellowship of the Ring",
-    overview: "Young hobbit Frodo Baggins, after inheriting a mysterious ring from his uncle Bilbo, must leave his home in order to keep it from falling into the hands of its evil creator.",
-    release_date: "2001-12-18",
-    vote_average: 8.3,
-    runtime: 178,
-    poster_path: "https://image.tmdb.org/t/p/w500/6oom5QYQ2yQTMJIbnvbkBL9cHo6.jpg",
-    
-  }
 
   return (
     <>
